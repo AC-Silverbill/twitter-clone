bced4b6bffd255f7ff871940a469e2f3cd6f79bb
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-09-23 01:59:44 -0500

    core: polish searcbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
-import React, { useRef } from "react";
+"use client";
+
+import React, { useRef, useState } from "react";
 import getLocal from "~/utils/getLocal";
 
 import { BsSearch } from "react-icons/bs";
 const SearchBar = () => {
+    const [value, setValue] = useState("");
     const secondaryColor = getLocal("colors", "COLOR_BORDER");
     const inputRef = useRef<HTMLInputElement>(null);
     const setFocus = (e: any) => {
@@ -12,11 +15,18 @@ const SearchBar = () => {
     return (
         <div className="p-2">
             <div
-                className={`group flex items-center bg-[${secondaryColor}] rounded-2xl p-2 focus:bg-white focus:translate-x-2 border-2 border-transparent focus-within:border-black`}
+                className={`group flex items-center bg-[${secondaryColor}] rounded-2xl p-2 focus-within:bg-white focus:translate-x-2 border-2 border-transparent focus-within:border-black`}
                 onClick={setFocus}
             >
                 <BsSearch className="w-10" />
-                <input ref={inputRef} type="text" placeholder="Search" className={`bg-transparent outline-none`} />
+                <input
+                    ref={inputRef}
+                    type="text"
+                    placeholder="Search"
+                    className={`bg-transparent outline-none`}
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
             </div>
         </div>
     );
