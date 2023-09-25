25664d29d8cfd6507e762c209c860cd4db99ca86
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-09-25 03:07:24 -0500

    fix: fixed index page popping up for a frame

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Redirect = () => {
-    return <div className="flex items-center justify-center">Redirecting to another page...</div>;
+    return (
+        <div className="flex justify-center items-center h-full">
+            <p>Redirecting to another page...</p>
+        </div>
+    );
 };
 
 export default Redirect;
