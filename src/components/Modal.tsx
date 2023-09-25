728498cb53c7ec5ed71d633aabf4b98454279631
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-09-25 04:24:28 -0500

    fix: removed styles that will be changed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,11 +19,11 @@ const Modal = ({ isOpen, onChange, title, description, children }: ModalProps) =
             <Dialog.Portal>
                 <Dialog.Overlay className="bg-white backdrop-blur-[1px] fixed inset-0" />
                 <Dialog.Content className="fixed h-[300px] w-[500px] border top-[50%] left-[50%]">
-                    <Dialog.Title className="text-xl text-center font-bold mb-4">{title}</Dialog.Title>
-                    <Dialog.Description className="mb-5 text-sm leading-normal text-center">{description}</Dialog.Description>
+                    <Dialog.Title className="">{title}</Dialog.Title>
+                    <Dialog.Description className="">{description}</Dialog.Description>
                     <div>{children}</div>
                     <Dialog.Close asChild>
-                        <button className="text-neutrl-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none">
+                        <button className="">
                             <IoMdClose />
                         </button>
                     </Dialog.Close>
