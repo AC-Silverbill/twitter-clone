9ee8f4698db5ec9d76e32ec15e273ae8f8d1ff55
LOE <max.shady1611@gmail.com>
2023-09-25 18:56:14 +0330

    core: createProfile as a prisma transaction

--- src/test/helpers/reset-db.ts
+++ src/test/helpers/reset-db.ts
@@ -1,11 +1,5 @@
 import { db } from "~/server/db";
 
 export const resetDB = async () => {
-    await db.$transaction([
-        db.example.deleteMany(),
-        db.account.deleteMany(),
-        db.session.deleteMany(),
-        db.user.deleteMany(),
-        db.verificationToken.deleteMany(),
-    ]);
+    await db.$transaction([db.account.deleteMany(), db.session.deleteMany(), db.user.deleteMany(), db.verificationToken.deleteMany()]);
 };
