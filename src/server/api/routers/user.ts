c3ab8e54f08f084410c30f6cf9ed1d6018599c01
LOE <max.shady1611@gmail.com>
2023-09-25 21:19:55 +0330

    core: added profile route queries

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,7 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 import { resetDB } from "~/test/helpers/reset-db";
+import { Profile } from "~/types";
 
 export const userRouter = createTRPCRouter({
     createProfile: publicProcedure
@@ -30,6 +31,29 @@ export const userRouter = createTRPCRouter({
                 }),
             ]);
         }),
+
+    getMe: protectedProcedure.query(async ({ ctx }) => {
+        return (await ctx.db.profile.findUniqueOrThrow({
+            where: {
+                id: ctx.session.user.id,
+            },
+        })) as Profile;
+    }),
+
+    getProfile: protectedProcedure
+        .input(
+            z.object({
+                id: z.string().cuid(),
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            return (await ctx.db.profile.findUniqueOrThrow({
+                where: {
+                    id: input.id,
+                },
+            })) as Profile;
+        }),
+
     resetDB: publicProcedure.mutation(async () => {
         await resetDB();
     }),
