cece06f325e5e560532893ab89ef4baa1de41154
LOE <max.shady1611@gmail.com>
2023-10-06 22:01:55 +0330

    core: finished tweet repository

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -12,74 +12,37 @@ export const tweetRouter = createTRPCRouter({
                 attachments: z.string().array().max(4),
             })
         )
-        .mutation(async ({ ctx, input: { content, attachments } }) => {
-            await ctx.db.tweet.create({
-                data: {
-                    authorUsername: ctx.profile.username,
-                    content,
-                    attachments,
-                },
-            });
+        .mutation(async ({ ctx, input }) => {
+            await ctx.repository.tweet.createTweet(input);
         }),
 
     postRetweet: protectedProcedure
         .input(
             z.object({
-                referenceId: z.string().cuid(),
+                retweetReferenceId: z.string().cuid(),
                 content: z.string(),
                 attachments: z.string().array().max(4),
             })
         )
         .mutation(async ({ ctx, input }) => {
-            const { referenceId, content, attachments } = input;
-            await ctx.db.tweet.create({
-                data: {
-                    authorUsername: ctx.profile.username,
-                    retweetReferenceId: referenceId,
-                    content,
-                    attachments,
-                    type: "RETWEET",
-                },
-            });
-            const retweetAuthor = await ctx.db.tweet.findUniqueOrThrow({
-                where: {
-                    id: referenceId,
-                },
-                select: {
-                    authorUsername: true,
-                },
-            });
-            await updateScore(ctx.db, ctx.profile.username, retweetAuthor.authorUsername, 20);
+            await ctx.repository.tweet.createRetweet(input);
+            const retweet = await ctx.repository.tweet.getTweet(input.retweetReferenceId);
+            if (retweet.authorUsername !== ctx.profile.username)
+                await updateScore(ctx.db, ctx.profile.username, retweet.authorUsername, 20);
         }),
 
     postReply: protectedProcedure
         .input(
             z.object({
-                referenceId: z.string().cuid(),
+                replyReferenceId: z.string().cuid(),
                 content: z.string(),
                 attachments: z.string().array().max(4),
             })
         )
         .mutation(async ({ ctx, input }) => {
-            const { referenceId, content, attachments } = input;
-            await ctx.db.tweet.create({
-                data: {
-                    authorUsername: ctx.profile.username,
-                    replyReferenceId: referenceId,
-                    content,
-                    attachments,
-                    type: "REPLY",
-                },
-            });
-            const tweetAuthor = await ctx.db.tweet.findUniqueOrThrow({
-                where: {
-                    id: referenceId,
-                },
-                select: {
-                    authorUsername: true,
-                },
-            });
-            await updateScore(ctx.db, ctx.profile.username, tweetAuthor.authorUsername, 20);
+            await ctx.repository.tweet.createReply(input);
+            const reply = await ctx.repository.tweet.getTweet(input.replyReferenceId);
+            if (reply.authorUsername !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, reply.authorUsername, 20);
         }),
 
     postLike: protectedProcedure
@@ -189,6 +152,7 @@ export const tweetRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input: { skip } }) => {
+            // TODO: maybe use cursor
             const topLatestTweets: TweetPayload[] = await ctx.db.tweet.findMany({
                 orderBy: {
                     likes: {
@@ -205,13 +169,8 @@ export const tweetRouter = createTRPCRouter({
     getTweet: protectedProcedure
         .input(z.object({ tweetId: z.string().cuid() }))
         .query(async ({ ctx, input: { tweetId } }): Promise<Tweet> => {
-            const tweet: TweetPayload = await ctx.db.tweet.findUniqueOrThrow({
-                where: {
-                    id: tweetId,
-                },
-                include: tweetInclude,
-            });
-            await updateScore(ctx.db, ctx.profile.username, tweet.authorUsername, 20);
+            const tweet = await ctx.repository.tweet.getTweet(tweetId);
+            if (tweet.authorUsername !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, tweet.authorUsername, 20);
             return tweetMapper(tweet);
         }),
 
@@ -222,21 +181,8 @@ export const tweetRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input: { username } }): Promise<Tweet[]> => {
-            const tweets: TweetPayload[] = await ctx.db.tweet.findMany({
-                where: {
-                    authorUsername: username,
-                    OR: [
-                        {
-                            type: "TWEET",
-                        },
-                        {
-                            type: "RETWEET",
-                        },
-                    ],
-                },
-                include: tweetInclude,
-            });
-            await updateScore(ctx.db, ctx.profile.username, username, 20);
+            const tweets: TweetPayload[] = await ctx.repository.tweet.getTweetsFromUsername(username);
+            if (username !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, username, 20);
             return tweets.map((tweet) => tweetMapper(tweet));
         }),
 
@@ -247,14 +193,8 @@ export const tweetRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input: { username } }): Promise<Tweet[]> => {
-            const replies: TweetPayload[] = await ctx.db.tweet.findMany({
-                where: {
-                    authorUsername: username,
-                    type: "REPLY",
-                },
-                include: tweetInclude,
-            });
-            await updateScore(ctx.db, ctx.profile.username, username, 20);
+            const replies: TweetPayload[] = await ctx.repository.tweet.getRepliesFromUsername(username);
+            if (username !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, username, 20);
             return replies.map((reply) => tweetMapper(reply));
         }),
 
@@ -286,16 +226,8 @@ export const tweetRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input: { username } }) => {
-            const mediaTweets: TweetPayload[] = await ctx.db.tweet.findMany({
-                where: {
-                    authorUsername: username,
-                    attachments: {
-                        isEmpty: false,
-                    },
-                },
-                include: tweetInclude,
-            });
-            await updateScore(ctx.db, ctx.profile.username, username, 20);
+            const mediaTweets: TweetPayload[] = await ctx.repository.tweet.getMediaFromUsername(username);
+            if (username !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, username, 20);
             return mediaTweets.map((tweet) => tweetMapper(tweet));
         }),
 
@@ -306,19 +238,9 @@ export const tweetRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input: { tweetId } }): Promise<Tweet[]> => {
-            const replies: TweetPayload[] = await ctx.db.tweet.findMany({
-                where: {
-                    replyReferenceId: tweetId,
-                    type: "REPLY",
-                },
-                include: tweetInclude,
-            });
-            const tweet = await ctx.db.tweet.findUniqueOrThrow({
-                where: {
-                    id: tweetId,
-                },
-            });
-            await updateScore(ctx.db, ctx.profile.username, tweet.authorUsername, 20);
+            const replies: TweetPayload[] = await ctx.repository.tweet.getRepliesFromTweet(tweetId);
+            const tweet = await ctx.repository.tweet.getTweet(tweetId);
+            if (tweet.authorUsername !== ctx.profile.username) await updateScore(ctx.db, ctx.profile.username, tweet.authorUsername, 20);
             return replies.map((reply) => tweetMapper(reply));
         }),
 });
--- src/server/repositories/TweetRepository.ts
+++ src/server/repositories/TweetRepository.ts
@@ -2,12 +2,103 @@ import { Repository } from "~/server/repositories/Repository";
 import { tweetInclude, type TweetPayload } from "~/server/data-model";
 
 export class TweetRepository extends Repository {
+    async createTweet(input: { content: string; attachments: string[] }) {
+        await this.db.tweet.create({
+            data: {
+                authorUsername: this.profile.username,
+                type: "TWEET",
+                ...input,
+            },
+        });
+    }
+
+    async createRetweet(input: { content: string; attachments: string[]; retweetReferenceId: string }) {
+        await this.db.tweet.create({
+            data: {
+                authorUsername: this.profile.username,
+                type: "RETWEET",
+                ...input,
+            },
+        });
+    }
+
+    async createReply(input: { content: string; attachments: string[]; replyReferenceId: string }) {
+        await this.db.tweet.create({
+            data: {
+                authorUsername: this.profile.username,
+                type: "REPLY",
+                ...input,
+            },
+        });
+    }
+
     async getTweet(tweetId: string): Promise<TweetPayload> {
         return await this.db.tweet.findUniqueOrThrow({
             where: {
                 id: tweetId,
             },
             include: tweetInclude,
+        });
+    }
+
+    async getRepliesFromTweet(tweetId: string): Promise<TweetPayload[]> {
+        return await this.db.tweet.findMany({
+            where: {
+                replyReferenceId: tweetId,
+                type: "REPLY",
+            },
+            include: tweetInclude,
+            orderBy: {
+                timeCreated: "desc",
+            },
+        });
+    }
+
+    async getTweetsFromUsername(username: string): Promise<TweetPayload[]> {
+        return await this.db.tweet.findMany({
+            where: {
+                authorUsername: username,
+                OR: [
+                    {
+                        type: "TWEET",
+                    },
+                    {
+                        type: "RETWEET",
+                    },
+                ],
+            },
+            include: tweetInclude,
+            orderBy: {
+                timeCreated: "desc",
+            },
+        });
+    }
+
+    async getRepliesFromUsername(username: string): Promise<TweetPayload[]> {
+        return await this.db.tweet.findMany({
+            where: {
+                authorUsername: username,
+                type: "REPLY",
+            },
+            include: tweetInclude,
+            orderBy: {
+                timeCreated: "desc",
+            },
+        });
+    }
+
+    async getMediaFromUsername(username: string): Promise<TweetPayload[]> {
+        return await this.db.tweet.findMany({
+            where: {
+                authorUsername: username,
+                attachments: {
+                    isEmpty: false,
+                },
+            },
+            include: tweetInclude,
+            orderBy: {
+                timeCreated: "desc",
+            },
         });
     }
 }
