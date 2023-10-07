edf77b0fb3223d960b0388bf7a308b1fd09f4bcc
LOE <max.shady1611@gmail.com>
2023-10-07 16:08:04 +0330

    core: fixed uploadthing

--- src/pages/api/uploadthing.ts
+++ src/pages/api/uploadthing.ts
@@ -1,10 +1,9 @@
 import { createNextPageApiHandler } from "uploadthing/next-legacy";
-import { env } from "~/env.mjs";
+
 import { ourFileRouter } from "~/server/uploadthing";
 
 const handler = createNextPageApiHandler({
     router: ourFileRouter,
-    config: { uploadthingSecret: env.UPLOADTHING_SECRET, uploadthingId: env.UPLOADTHING_APP_ID },
 });
 
 export default handler;
