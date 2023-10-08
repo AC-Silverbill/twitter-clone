6b61ac82d32c9c65e5d2b71a8c9cbf277759eaf4
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-10-08 12:22:41 -0500

    core: my changelog

--- src/components/messaging/UploadFile.tsx
+++ src/components/messaging/UploadFile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Icon from "../Icon";
 import { FaRegImage } from "react-icons/fa6";
 import getLocals from "~/utils/getLocals";
+import { UploadButton } from "~/utils/uploadthing";
 
 interface UploadImageProps {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -9,12 +10,42 @@ interface UploadImageProps {
 const UploadImage = ({ onChange }: UploadImageProps) => {
     const { COLOR_PRIMARY } = getLocals("colors");
     return (
-        <div className="flex justify-center items-center">
-            <label htmlFor="upload-image">
-                <Icon key={"addfile"} className="cursor-pointer">
-                    <FaRegImage className={`text-${COLOR_PRIMARY} scale-y-125`} />
-                </Icon>
-            </label>
+        <div className="group p-2 flex justify-center items-center rounded-full hover:bg-#e8f5fe scale-100 cursor-pointer">
+            <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                    // Do something with the response
+                    console.log("Files: ", res);
+                    alert("Upload Completed");
+                }}
+                onUploadError={(error: Error) => {
+                    // Do something with the error.
+                    alert(`ERROR! ${error.message}`);
+                }}
+                className="p-2"
+                appearance={{
+                    button: {
+                        background: "transparent",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                        alignSelf: "center",
+                    },
+                    container: {
+                        padding: "0px",
+                        width: 20,
+                        height: 20,
+                        flexDirection: "row",
+                        gap: "0px",
+                        outline: "none",
+                        border: "0px",
+                    },
+                }}
+                content={{
+                    button: <FaRegImage className={`text-${COLOR_PRIMARY} scale-y-125`} />,
+                    allowedContent: <></>,
+                }}
+            />
             <input id="upload-image" type="file" className="w-0 h-0" accept="image/*" onChange={onChange} />
         </div>
     );
