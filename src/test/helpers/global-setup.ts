c053a7713c9d8b639537965ceecdf2f4b2c59743
LOE <max.shady1611@gmail.com>
2023-10-10 18:12:45 +0330

    core: setting a standalone server for tests

--- src/test/helpers/global-setup.ts
+++ src/test/helpers/global-setup.ts
@@ -1,3 +1,25 @@
+import type * as http from "http";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { appRouter } from "~/server/api/root";
+import { createInnerTRPCContext } from "~/server/api/trpc";
+
 import("~/env.mjs");
 
-export const setup = async () => {};
+const globalThisWithServer = globalThis as unknown as {
+    server: http.Server;
+};
+
+export const setup = () => {
+    const { server } = createHTTPServer({
+        router: appRouter,
+        createContext: () => {
+            return createInnerTRPCContext({ session: null });
+        },
+    });
+    globalThisWithServer.server = server;
+    server.listen(3000);
+};
+
+export const teardown = () => {
+    if (globalThisWithServer.server) globalThisWithServer.server.close();
+};
