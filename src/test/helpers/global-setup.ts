18050dfa9dab01884cdc427e09f93878d276ccb0
LOE <max.shady1611@gmail.com>
2023-10-10 18:27:02 +0330

    core: no I'm not using the standalone server

--- src/test/helpers/global-setup.ts
+++ src/test/helpers/global-setup.ts
@@ -1,25 +1,3 @@
-import type * as http from "http";
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
-import { appRouter } from "~/server/api/root";
-import { createInnerTRPCContext } from "~/server/api/trpc";
-
 import("~/env.mjs");
 
-const globalThisWithServer = globalThis as unknown as {
-    server: http.Server;
-};
-
-export const setup = () => {
-    const { server } = createHTTPServer({
-        router: appRouter,
-        createContext: () => {
-            return createInnerTRPCContext({ session: null });
-        },
-    });
-    globalThisWithServer.server = server;
-    server.listen(3000);
-};
-
-export const teardown = () => {
-    if (globalThisWithServer.server) globalThisWithServer.server.close();
-};
+export const setup = () => {};
